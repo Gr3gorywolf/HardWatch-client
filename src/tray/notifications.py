591f59853c101d55218d3693f3ed8a9937c3c278
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2025-07-12 16:32:33 -0400

    catched notifications in case that is unsopported

--- src/tray/notifications.py
+++ src/tray/notifications.py
@@ -1,12 +1,18 @@
 from notifypy import Notify
 import config
+
+
 def show_notification(title, message):
-    if(config.DISABLE_NOTIFICATIONS):
+    if config.DISABLE_NOTIFICATIONS:
         return
-    notification = Notify(
-        default_notification_application_name="HardWatch",
-        default_notification_icon="icon.ico",
-    )
-    notification.title = title
-    notification.message = message
-    notification.send()
+    try:
+        notification = Notify(
+            default_notification_application_name="HardWatch",
+            default_notification_icon="icon.ico",
+        )
+        notification.title = title
+        notification.message = message
+        notification.send()
+    except Exception as e:
+        print(f"Failed to send notification: {e}")
+        return
